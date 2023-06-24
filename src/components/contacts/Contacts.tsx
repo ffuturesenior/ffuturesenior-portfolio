24d6d6c9fc23660ab6eb1ec2737766eb2c9a9cc5
ffuturesenior <shamray.ruslan068@gmail.com>
2023-06-24 13:48:57 +0300

    f

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -29,7 +29,7 @@ export const Contacts=()=>{
                             </div>
                         </div>
                         <div className='contacts_map'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d745.178281406245!2d13.19789385476288!3d49.712375885571014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470a93242bf7e5a3%3A0xe5d2d16f0364e3b0!2zUG9oxZllYm7DrSBzbHXFvmJhIEhhbmEgVmFuacWhb3bDoQ!5e0!3m2!1sru!2scz!4v1684412289616!5m2!1sru!2scz" width="400" height="300" style={{border:"0"}} allowFullScreen={undefined} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1116.505476460234!2d13.197415091213015!3d49.71239833423742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470a93242bf7e5a3%3A0xe5d2d16f0364e3b0!2zUG9oxZllYm7DrSBzbHXFvmJhIEhhbmEgVmFuacWhb3bDoQ!5e0!3m2!1sru!2scz!4v1687537384317!5m2!1sru!2scz" width="400" height="300" style={{border:0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                     <div className='contacts_body_form shadow'>
--- src/components/contacts/form.tsx
+++ src/components/contacts/form.tsx
@@ -57,25 +57,25 @@ export const Form=()=>{
 
     return(
         <>
-        <form onSubmit={(e)=>submit(e)} className='form_block'>
+        <form onSubmit={(e)=>submit(e)} className='form_block p-3' style={{minWidth:"300px"}}>
             <div className="row ">
                 <div className="col-md-6" style={{}}>
                     <label htmlFor="validationDefault01" className="form-label">Your name</label>
-                    <input onChange={e=>nameInputHandler(e.target.value)} type="text" className="form-control" id="validationDefault01" value={message.name} required/>
+                    <input  onChange={e=>nameInputHandler(e.target.value)} type="text" className="form-control" id="validationDefault01" value={message.name} required/>
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="validationDefault02" className="form-label">Your email</label>
-                    <input onChange={e=>emailInputHandler(e.target.value)} type="text" className="form-control" id="validationDefault02" value={message.email} required/>
+                    <input  onChange={e=>emailInputHandler(e.target.value)} type="text" className="form-control" id="validationDefault02" value={message.email} required/>
                 </div>
             </div>
             <br/>
             <div >
                 <label htmlFor="validationDefaultUsername" className="form-label">subject</label>
-                <input onChange={e=>subjectInputHandler(e.target.value)} type="text" className="form-control" id="validationDefaultUsername" value={message.subject} aria-describedby="inputGroupPrepend2" required/> 
+                <input  onChange={e=>subjectInputHandler(e.target.value)} type="text" className="form-control" id="validationDefaultUsername" value={message.subject} aria-describedby="inputGroupPrepend2" required/> 
             </div>
             <div className="">
                 <label htmlFor="validationDefault03" className="form-label">message</label>
-                <textarea onChange={e=>messageInputHandler(e.target.value)} className="form-control" value={message.message} id="validationDefault03" required/>
+                <textarea  onChange={e=>messageInputHandler(e.target.value)} className="form-control" value={message.message} id="validationDefault03" required/>
             </div> 
             <div className="text-center">
                 <button className={`btn btn-${isLoading?`warning`:`primary`}`} type="submit">{buttonText}</button>
